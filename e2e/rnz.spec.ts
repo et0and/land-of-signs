63ea1bb9626aa36a3ee2b4e543e6fe63771df772
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-05-28 11:17:12 +1200

    Update rnz.spec.ts

--- e2e/rnz.spec.ts
+++ e2e/rnz.spec.ts
@@ -6,8 +6,8 @@ test("RNZ", async ({ page }) => {
   test.slow();
   await page.goto("https://www.rnz.co.nz/");
   await page.waitForTimeout(3000);
-  await page.locator("div.o-digest--news").first().click();
-  const headline = await page.locator("h1").innerText();
+  await page.locator("#documentContent > div > div.t-home-top > div > div.c-top-stories__primary.primary.layout--lead_story.has-secondary.item-count--3 > div > div.o-digest__detail > h3 > a").first().click();
+  const headline = await page.locator("#documentContent > div.content__primary.u-divider-bottom\@until-medium > div > header > h1").innerText();
 
   // Write the headline to a file
   const filePath = path.join(process.cwd(), "data", "rnz.txt");
